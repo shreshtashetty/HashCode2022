2d38810d132e947aedb960c1e27924d3870051fe
Shreshta Shetty <shreshtas94@gmail.com>
2022-02-14 21:48:08 +0530

    Added score calculator

--- onePizza/main.py
+++ onePizza/main.py
@@ -1,6 +1,7 @@
 import sys
 from fileReader import *
 from fileWriter import *
+from scoreCalculator import *
 
 
 # PATH = r'C:\Users\shreshta\Downloads\Hashcode\input_data\e_elaborate.in.txt'
@@ -22,5 +23,8 @@ if __name__ == '__main__':
     like, dislike = fileReader(PATH)
     res = optimize(like, dislike)
     # print(res)
+    score = scoreCalc(res, PATH)
+    # print(score)
     fileWriter(res)
+
 
